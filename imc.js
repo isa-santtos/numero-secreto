1a4810b2aff8dc17964d218816a5dc3e77a824a4
isa-santtos <isabody@gmail.com>
2024-05-08 10:08:30 -0300

    projeto inicial

---
+++ imc.js
@@ -0,0 +1,11 @@
+// Crie uma função que calcule o índice de massa corporal (IMC) de uma pessoa,
+// a partir de sua altura, em metros, e peso, em quilogramas, 
+// que serão recebidos como parâmetro.
+
+function calculaIMC(altura, peso){
+
+    let imc = peso / (alturaMetros * alturaMetros);
+    
+    return imc;
+}
+
